d79cfee306980c69ea171e65a25a7df9d93716a4
Nick Dieff <nick.dieff@gmail.com>
2022-10-11 19:32:52 -0400

    Task 2: add HTML for a header, a list, and an image

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-            </p>
+            <h1 className="App-header">UM COS420: Task 2</h1>
+            <img
+                src="https://highered.aspeninstitute.org/wp-content/uploads/2022/06/univ-maine-system.jpeg"
+                alt="The University of Maine, from above"
+            />
+            <p>Hello World</p>
+            <p>Steps to complete this task</p>
+            <ul>
+                <li>Add basic HTML elements</li>
+                <li>Add CSS</li>
+                <li>Add a button</li>
+                <li>Create a two-column layout</li>
+                <li>Red tectangle</li>
+            </ul>
         </div>
     );
 }
