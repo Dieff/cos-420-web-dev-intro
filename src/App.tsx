bb8254f9ee69afd14e7f9a8e27424bc71d973fed
Nick Dieff <nick.dieff@gmail.com>
2022-10-11 19:44:30 -0400

    Add red rectangle and place all elements in two column bootstrap layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function logButton() {
@@ -15,16 +15,26 @@ function App(): JSX.Element {
                 src="https://highered.aspeninstitute.org/wp-content/uploads/2022/06/univ-maine-system.jpeg"
                 alt="The University of Maine, from above"
             />
-            <p>Hello World</p>
-            <p>Steps to complete this task</p>
-            <ul>
-                <li>Add basic HTML elements</li>
-                <li>Add CSS</li>
-                <li>Add a button</li>
-                <li>Create a two-column layout</li>
-                <li>Red tectangle</li>
-            </ul>
-            <Button onClick={logButton}>Log Hello World</Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>Steps to complete this task</p>
+                        <ul>
+                            <li>Add basic HTML elements</li>
+                            <li>Add CSS</li>
+                            <li>Add a button</li>
+                            <li>Create a two-column layout</li>
+                            <li>Red tectangle</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <p>Hello World</p>
+                        <Button onClick={logButton}>Log Hello World</Button>
+
+                        <div className="Red-rect" />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
