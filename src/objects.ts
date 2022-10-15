0e244ba9acd1dabb391eabc400fed2fbb8ee25ad
Nick Dieff <nick.dieff@gmail.com>
2022-10-15 09:52:59 -0400

    implemented object functions: `duplicateQuestion` and `addOption`

--- src/objects.ts
+++ src/objects.ts
@@ -102,7 +102,7 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return { ...question, published: !question.published };
 }
 
 /**
@@ -112,7 +112,12 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        ...oldQuestion,
+        id,
+        published: false,
+        name: `Copy of ${oldQuestion.name}`
+    };
 }
 
 /**
@@ -123,7 +128,10 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    return {
+        ...question,
+        options: [...question.options, newOption]
+    };
 }
 
 /**
