3308f866a64d2c6d11804b12930529b944daa9ac
Nick Dieff <nick.dieff@gmail.com>
2022-10-18 14:26:45 -0400

    Fixed DoubleHalf component
    Removed the `DoubleHalfState` file. This implementation
    was problematic because you cannot call a React hook
    from outside of a component.

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,39 @@
-import React, { useState } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
-    return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
+interface ButtonProps {
+    setDhValue: Dispatch<SetStateAction<number>>;
 }
 
-function Halver(): JSX.Element {
-    return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
+function Doubler({ setDhValue }: ButtonProps): JSX.Element {
+    return (
+        <Button onClick={() => setDhValue((dhValue) => 2 * dhValue)}>
+            Double
+        </Button>
+    );
 }
+
+function Halver({ setDhValue }: ButtonProps): JSX.Element {
+    return (
+        <Button onClick={() => setDhValue((dhValue) => 0.5 * dhValue)}>
+            Halve
+        </Button>
+    );
+}
+
+const INITIAL_DOUBLE_HALF_VALUE = 10;
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState(INITIAL_DOUBLE_HALF_VALUE);
+
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler setDhValue={setDhValue}></Doubler>
+            <Halver setDhValue={setDhValue}></Halver>
         </div>
     );
 }
