d1cbd4f8560374cf06ee3ab136033f1cfbe5fdda
Nick Dieff <nick.dieff@gmail.com>
2022-11-17 14:49:47 -0500

    implmeent Changecolor and MultipleChoiceQuestion

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const COLORS = [
+    "darkcyan",
+    "hotpink",
+    "lemonchiffon",
+    "salmon",
+    "yellowgreen",
+    "springgreen",
+    " mediumslateblue",
+    "lavenderblush",
+    "aquamarine"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>(COLORS[0]);
+
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Group>
+                {COLORS.map((c) => (
+                    <Form.Check
+                        type="radio"
+                        key={c}
+                        onChange={() => setColor(c)}
+                        label={c}
+                        value={c}
+                        checked={c == color}
+                        inline
+                    />
+                ))}
+            </Form.Group>
+            <span>
+                You have chosen{" "}
+                <span
+                    data-testid="colored-box"
+                    style={{ backgroundColor: color, padding: "0.2rem" }}
+                >
+                    {color}
+                </span>
+                .
+            </span>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,33 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [curSelection, setCurSelection] = useState<string>(options[0]);
+    const status = useMemo(() => {
+        if (curSelection === expectedAnswer) {
+            return "✔️";
+        }
+        return "❌";
+    }, [curSelection, expectedAnswer]);
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="userEmotions">
+                <Form.Label>How do you feel?</Form.Label>
+                <Form.Select
+                    value={curSelection}
+                    onChange={(event) => {
+                        setCurSelection(event.target.value);
+                    }}
+                >
+                    {options.map((opt) => (
+                        <option key={opt} value={opt}>
+                            {opt}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {status}
         </div>
     );
 }
