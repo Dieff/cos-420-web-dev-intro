ec95fa0936963bf6eaedc6335e72ac07f0fe0cb8
Nick Dieff <nick.dieff@gmail.com>
2022-11-17 08:26:55 -0500

    fix ColoredBox component

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,39 +4,47 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
-    return (
-        <Button onClick={() => setColorIndex((1 + colorIndex) % COLORS.length)}>
-            Next Color
-        </Button>
-    );
+interface ChangeColorProps {
+    changeState: () => void;
 }
 
-function ColorPreview(): JSX.Element {
+const ChangeColor: React.FC<ChangeColorProps> = (props) => {
+    return <Button onClick={() => props.changeState()}>Next Color</Button>;
+};
+
+interface ColorPreviewProps {
+    index: number;
+}
+
+const ColorPreview: React.FC<ColorPreviewProps> = (props) => {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[props.index],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px"
             }}
         ></div>
     );
-}
+};
 
 export function ColoredBox(): JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+    const updateIndex = () => {
+        setColorIndex((index) => (index + 1) % COLORS.length);
+    };
+
     return (
         <div>
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor changeState={updateIndex}></ChangeColor>
+                <ColorPreview index={colorIndex}></ColorPreview>
             </div>
         </div>
     );
